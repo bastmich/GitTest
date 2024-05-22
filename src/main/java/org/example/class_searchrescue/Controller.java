fd9554662069c4dc2fa74946a95dd1777a1de4a5
bastmich <bastienmicheloud@gmail.com>
2024-05-22 16:58:15 +0200

    Test

---
+++ src/main/java/org/example/class_searchrescue/Controller.java
@@ -0,0 +1,10 @@
+package org.example.class_searchrescue;
+
+public class Controller {
+    protected void StartSim(){};
+
+    protected void StopSim(){};
+    protected void ResetSim(){};
+    protected void LoadConfig(){};
+
+}
