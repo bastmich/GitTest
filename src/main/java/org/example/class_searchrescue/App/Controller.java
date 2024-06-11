349e640152d1f2a3e7fbcf7872ea2573bf1d4af9
florpral <florpral03@gmail.com>
2024-06-11 11:37:09 +0200

    Adds Mar 11 1140

--- src/main/java/org/example/class_searchrescue/App/Controller.java
+++ src/main/java/org/example/class_searchrescue/App/Controller.java
@@ -15,41 +15,42 @@ import java.io.IOException;
 import javax.swing.JFileChooser;
 
 
-
+/**
+ * Controller class for handling the user interface events and interactions for the Search and Rescue application.
+ */
 public class Controller {
 
+    //Process variables
    private boolean running = false;
    private boolean pause = false;
 
-
-
+    // Images for different types of agents
     private Image agentMan = new Image(SimController.class.getResourceAsStream("/Image/Agent/human.png"));
     private Image agentHelicopter = new Image(SimController.class.getResourceAsStream("/Image/Agent/helicopter.png"));
     private Image agentDrone = new Image(SimController.class.getResourceAsStream("/Image/Agent/drone.png"));
 
-
+    // Variables for configuration file parameters
     private int targetPositionxFile;
     private int targetPositionyFile;
     private float agentsSpeedFile;
     private int agentsDetectionRangeFile;
     private int agentsCommunicationRangeFile;
 
-
-
+    // FXML annotated user interface components
     @FXML Label targetPositionX;
     @FXML Label targetPositionY;
     @FXML Label agentSpeed;
     @FXML Label agentDetectionRange;
     @FXML Label agentCommunicationRange;
     @FXML Label uploadState;
-
     @FXML Slider slider;
-
     @FXML Rectangle SquareMan;
     @FXML Rectangle SquareHelicopter;
     @FXML Rectangle SquareDrone;
 
-
+    /**
+     * Initializes the controller class. This method is automatically called after the FXML file has been loaded.
+     */
  @FXML
  private void initialize() {
   SquareMan.setVisible(true);
@@ -63,6 +64,11 @@ public class Controller {
   });
 
  }
+
+    /**
+     * Updates the slider value and the number of agents.
+     * @param newValue the new value for the slider.
+     */
     private void updateSlider(int newValue)
     {
         if(!running ||SimController.actualNumberOfFound== SimController.numberOfFounded)
@@ -76,6 +82,9 @@ public class Controller {
         }
     }
 
+    /**
+     * Starts the simulation.
+     */
     @FXML
     private void startSim(){
 
@@ -91,8 +100,6 @@ public class Controller {
         {
             SimController.agentImage=agentHelicopter;
         }
-
-
 
      if (!running && SimController.actualNumberOfFound<SimController.numberOfFounded) {
 
@@ -113,8 +120,11 @@ public class Controller {
      {
          Application.simController.resetSim();
      }
-
     }
+
+    /**
+     * Stops the simulation.
+     */
     @FXML
     private void stopSim(){
         if (running) {
@@ -123,9 +133,12 @@ public class Controller {
         }
     }
 
+
+    /**
+     * Resets the simulation.
+     */
     @FXML
     private void resetSim(){
-
 
         if(SquareMan.isVisible())
         {
@@ -144,10 +157,12 @@ public class Controller {
         updateConfigLabel();
     }
 
-
+    /**
+     * Loads the configuration from a file.
+     */
     @FXML
     private void loadConfig() {
-        // Ouvrir le sélecteur de fichiers
+        // Open the file selection windows
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
         int result = fileChooser.showOpenDialog(null);
@@ -156,16 +171,17 @@ public class Controller {
             File selectedFile = fileChooser.getSelectedFile();
             File configFile = new File(selectedFile.getAbsolutePath());
 
-            // Vérifier si le fichier existe
+            // Check if the file exist
             if (!configFile.exists() || !configFile.isFile()) {
                 System.out.println("Ce fichier n'existe pas : " + selectedFile.getAbsolutePath());
                 return;
             }
 
-            // Utiliser BufferedReader
+            // UseBufferedReader
             try (BufferedReader br = new BufferedReader(new FileReader(configFile))) {
                 String line;
-                // Lire le fichier
+
+                // Read the file
                 while ((line = br.readLine()) != null) {
                     String[] parts = line.split(" ");
                     switch (parts[0]) {
@@ -184,6 +200,7 @@ public class Controller {
                             break;
                     }
                 }
+                //Update agents and set correct texts
                 uploadState.setText("File uploaded successfully");
                 Application.simController.updateCongigFile(agentsSpeedFile,agentsCommunicationRangeFile,agentsDetectionRangeFile,targetPositionxFile,targetPositionyFile);
                 updateConfigLabel();
@@ -194,9 +211,6 @@ public class Controller {
                     agentCommunicationRange.setText(Float.toString(agentsCommunicationRangeFile));
                 }
 
-
-
-
             } catch (IOException e) {
                 System.out.println("Erreur lors de la lecture du fichier de configuration : " + e.getMessage());
             }
@@ -206,22 +220,24 @@ public class Controller {
         }
     }
 
+    /**
+     * Changes the agent's image to a man.
+     */
     @FXML
     private void changeImageMan(){
-
      for(int i=0;i<SimController.agents.size();i++)
      {
        SimController.agents.get(i).changeImage(agentMan);
      }
-     System.out.println("man");
-
      //Visibility management
-
-            SquareMan.setVisible(true);
-            SquareHelicopter.setVisible(false);
-            SquareDrone.setVisible(false);
-
+        SquareMan.setVisible(true);
+        SquareHelicopter.setVisible(false);
+        SquareDrone.setVisible(false);
     }
+
+    /**
+     * Changes the agent's image to a helicopter.
+     */
     @FXML
     private void changeImageHelicopter(){
      for(int i=0;i<SimController.agents.size();i++)
@@ -229,27 +245,29 @@ public class Controller {
       SimController.agents.get(i).changeImage(agentHelicopter);
      }
      //Visibility management
-
-            SquareMan.setVisible(false);
-            SquareHelicopter.setVisible(true);
-            SquareDrone.setVisible(false);
-
+        SquareMan.setVisible(false);
+        SquareHelicopter.setVisible(true);
+        SquareDrone.setVisible(false);
     }
+
+    /**
+     * Changes the agent's image to a drone.
+     */
     @FXML
     private void changeImageDrone(){
      for(int i=0;i<SimController.agents.size();i++)
      {
       SimController.agents.get(i).changeImage(agentDrone);
      }
-
      //Visibility management
-
-            SquareMan.setVisible(false);
-            SquareHelicopter.setVisible(false);
-            SquareDrone.setVisible(true);
-
+        SquareMan.setVisible(false);
+        SquareHelicopter.setVisible(false);
+        SquareDrone.setVisible(true);
     }
 
+    /**
+     * Updates the configuration labels in the GUI with the current simulation values.
+     */
     private void updateConfigLabel()
     {
         targetPositionX.setText(Float.toString(SimController.target.getPositionX()+50));
@@ -257,9 +275,5 @@ public class Controller {
         agentSpeed.setText(Float.toString(SimController.agents.getFirst().getVelocity()));
         agentDetectionRange.setText(Float.toString(SimController.agents.getFirst().getRadiusDetection()));
         agentCommunicationRange.setText(Float.toString(SimController.agents.getFirst().radiusCommunication()));
-
     }
-
-
-
 }
